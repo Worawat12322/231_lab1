c5f43d52c77043145bddc7e39aa46556048af557
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-02-04 16:20:30 +0700

    update

--- 3-11 (three-eleven).cpp
+++ 3-11 (three-eleven).cpp
@@ -3,8 +3,5 @@ int main(){
 	int N;
 	scanf("%d",&N);
 	printf("%d %d",N%3,N%11);
-	unsigned int N;
-	scanf("%u",&N);
-	printf("%u %u",N%3,N%11);
 	return 0;
 }
--- Max N.cpp
+++ Max N.cpp
@@ -8,9 +8,10 @@ int main(){
 	{
 		scanf("%d",&num[i]);
 	}
-	for(i=0;i<N;i++)
+	int l;
+	for(l=0;l<N;l++)
 	{
-		printf("%d ",num[i]);
+		printf("%d",num[l]);
 	}
 	return 0;
 }
